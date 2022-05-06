27fc5cf9ad036d0a68f567b560c2cd889ddf6a62
kream77 <90008516+kream77@users.noreply.github.com>
2022-05-06 15:51:16 -0500

    changed form validation to PHP; added AJAX

--- profile.php
+++ profile.php
@@ -1,144 +1,90 @@
 <?php
-# get the incoming information
-  $name  = $_POST["username"];
-  $password  = $_POST["password"];
 
-  # open file 'users.txt' and append the name and e-mail address
-  if ($fh = fopen ("users.txt", "a"))
-  {
-    fwrite ($fh, "$name  $password\n");
-    fclose ($fh);
-  }
+$username = $_GET["username"];
+$password = $_GET["password"];
+$repeat = $_GET["repeat"];
 
-  # Write thank you page
-print <<<REGISTRATION_RESULT
-<html>
-  <head>
-   <title>Profile - Howdy, Austin</title>
-   <meta charset="UTF-8">
-   <meta name="description"
-   <meta name="author" content="Hana Bredstein, Mariana Herreria, Kaitlyn Ream, Hayley Slotboom">
-   <link href="home.css" rel="stylesheet">
-   <link rel="ICON" href="DISCO.png">
+register();
 
-</head>
-<body>
-<div class="top">
-	<a href="home.html">
-    <img src="HOWDY-AUSTIN.png" width="475" height="100">
-    </a>
+function validateUser() {
+	if (strlen($username < 6) || (strlen($username > 20)) {
+		return False;
+	} else {
+		return True;
+	}
+}
 
+function validatePass() {
+	if (strlen($password < 6) || (strlen($password > 20)) {
+		return False;
+	} else {
+		return True;
+	}
+}
 
-        <div class="navbar">
-                        <button style="margin-left: 29px; float: left" class="dropbtn"><a  id = "selectednav" href="home.html">HOME</a></button>
+function validateRepeat() {
+	if ($password == $repeat) {
+		return True;
+	} else {
+		return False;
+	}
+}
 
-                        <div class="dropdown">
-                                <button style="margin-left: 29px;" class="dropbtn"><a id="pink" href= "foodanddrink.html">FOOD & DRINK</a></button>
+function validateForm() {
+	if (validateUser() == TRUE && validatePass() == TRUE && validateRepeat() == TRUE) {
+		$validated = True;
+		return validated;
+	} elseif (validateUser() == FALSE) {
+		$validated = "user"
+		return $validated;
+	} elseif (validatePass() == FALSE) {
+		$validated = "pass"
+		return $validated;
+	} elseif (validateRepeat() == FALSE) {
+		$validated = "repeat"
+		return $validated;
+	}
 
-                                <div class="dropdown-content">
-                                        <a href="foodanddrink.html">Restaurants</a>
-                                        <a href="foodanddrink.html#coffeeshops">Coffee Shops</a>
-                                <a href="foodanddrink.html#bobatea">Boba Tea</a>
-                                </div>
-                        </div>
-
-                        <div class="dropdown">
-                                <button style="margin-left: 29px;" class="dropbtn"><a href="outdoors.html">OUTDOORS</a></button>
-
-                                <div class="dropdown-content">
-                                        <a href="outdoors.html">Hiking</a>
-                                        <a href="outdoors.html#swimming">Swimming</a>
-                                        <a href="outdoors.html#kayaking">Kayaking</a>
-                                <a href="outdoors.html#rockclimbing">Rock Climbing</a>
-                                </div>
-                        </div>
-
-                        <div class="dropdown">
-                                <button style="margin-left: 29px;" class="dropbtn"><a id="pink" href = "nightout.html">NIGHT OUT</a></button>
-
-                                <div class="dropdown-content">
-                                        <a href="nightout.html">Bars</a>
-                                        <a href="nightout.html#clubs">Clubs</a>
-                                </div>
-                        </div>
-
-                        <div class="dropdown">
-                                <button style="margin-left: 29px;" class="dropbtn"><a href = "campus.html">CAMPUS</a></button>
-
-                                <div class="dropdown-content">
-                                        <a href="campus.html">Study Places</a>
-                                </div>
-                        </div>
-
-                        <div class="dropdown">
-                                <button style="margin-left: 29px;" class="dropbtn"><a id="pink" href ="shopping.html">SHOPPING</a></button>
-
-                                <div class="dropdown-content">
-                                        <a href="shopping.html">Thrift Stores</a>
-                                </div>
-                        </div>
-
-                        <div class="dropdown">
-                                <button style="margin-left: 29px;" class="dropbtn"><a href= "events.html">EVENTS</a></button>
-
-                                <div class="dropdown-content">
-                                        <a href="events.html">Music</a>
-                                        <a href="events.html#sports">Sports</a>
-                                </div>
-                        </div>
-
-                        <button style="margin-left: 29px;" class="dropbtn"><a id="pink" href="contact.html">CONTACT US</a></button>
-
-                        <button style="margin-left: 29px;" class="dropbtn"><a href= "aboutus.html">ABOUT US</a></button>
+}
 
 
-                </div>
-	</div>
+function register() {
+	$validated = validateForm() 
+	
+	if ($validated == TRUE) {		
+		run_sql();
+	} else if ($validated == "user") {
+		echo "<p align='center'> <font color='#004280'> Username does not meet requirements </font></p>";
+	} else if ($validated == "pass") {
+		echo "<p align='center'> <font color='#004280'> Password does not meet requirements </font></p>";
+	} else if ($validated == "repeat") {
+		echo "<p align='center'> <font color='#004280'> Passwords do not match </font></p>";
+	}
+}
 
 
-<br>
-<br>
+function run_sql() {
 
-<p id="instructions">
-<b><i><u>Thank you for registering! Please log in below.</p>
+	$server = "spring-2022.cs.utexas.edu";
+	$my_user = "cs329e_bulko_mariana";
+	$my_password = "derby6crude6divine";
+	$dbName = "cs329e_bulko_mariana";
+
+	$mysqli = new mysqli ($server, $my_user, $my_password, $dbName);
+
+	$command = "SELECT * FROM howdyusers WHERE user = \"$username\"";
+	$result = $mysqli -> query($command);
 
 
-<form>
+		
+	if ($result->num_rows > 0) {
+		echo "<p align='center'> <font color='#004280'> Username already exists </font></p>";
 
-
-   <table id="contact-form" frame="box">
-      <tr >
-         <td id="label">Username: </td>
-         <td><input id="username" name = "username" type = "text" placeholder="Enter username..." width="40" /></td>
-      </tr>
-
-      <tr >
-         <td id="label">Password: </td>
-         <td><input id="password" name = "password" type = "password" placeholder="Enter password..." width="40"/></td>
-      </tr>
-
-      <tr >
-         <td id="label">Repeat password: </td>
-         <td><input id="repeat" name = "repeat" type = "password" placeholder="Repeat password..." width="40" /> </td>
-      </tr>
-
-      <tr>
-         <td><br></td>
-      </tr>
-
-      <tr>
-         <td id="buttons"> <input id="register" type="button" value="Register"/>
-             <input id="clear" type="reset" value="Clear"/> </td>
-      </tr>
-
-</table>
-
-</form>
-
-        <div class="bottom-info">
-                <p>CONTACT US <br>© HAYLEY SLOTBOOM, MARIANA HERRERIA, KAITLYN REAM, HANA BREDSTEIN 02/16/2022</p>
-        </div>
-</body>
-</html>
-REGISTRATION_RESULT;
+	} else {
+		$command = "INSERT INTO howdyusers VALUES ('$username', '$password')";
+		$mysqli -> query($command);
+		
+		echo "<p align='center'> <font color='#004280'> Thank you for registering! </font></p>";
+	}
+}
 ?>
