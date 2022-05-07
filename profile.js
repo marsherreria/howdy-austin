ceb0104a48e5e0f2e0a26a681b8164bb6c914668
kream77 <90008516+kream77@users.noreply.github.com>
2022-05-06 23:23:36 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,5 +1,3 @@
-// javascript file for profile.html
-
 var user = '';
 var password = '';
 var repeatPassword = '';
@@ -15,12 +13,19 @@ var passwordValidated = false;
 register = document.getElementById("register");
 register.addEventListener("click", registerAlert);
 
+clear = document.getElementById("clear");
+clear.addEventListener("click", clear_page);
+
+
+function clear_page() {
+	location.reload();
+}
+
 
 function getInfo() {
-    user = document.getElementById("username").value;
-    password = document.getElementById("password").value;
-    repeatPassword = document.getElementById("repeat").value;
-    return;
+	user = document.getElementById("username").value;
+	password = document.getElementById("password").value;
+	repeatPassword = document.getElementById("repeat").value;
 }
 
 // username must be 6-20 digits
@@ -33,111 +38,121 @@ function getInfo() {
 
 
 function registerAlert() {
-    if (validateForm()) {
-        alert("User validated");
-        register.type = "submit";
-        return;
-    } else {
-        alert("Invalid username or password");
-        return;
-        }
-}
+	validated = validateForm();
 
-
-function validateForm() {
-    getInfo();
-
-    if (validateUser() && validatePass() && validateRepeat()) {
-        return true;
-    } else {
-    	return false;
-    }
-
-}
-
-function validateUser() {
-    // check length of username
-    if ((user.length < 6) || (user.length > 20)) {
-        return false;
-    }
-
-    // check if only letters & digits
-    for (var i = 0; i < user.length; i++) {
-        charCode = user.charCodeAt(i);
-
-    	// checks if character is number
-    	if ((charCode < 57) && (charCode > 48)) {
-        	// check first character of user
-        	if (i = 0) {
-            	console.log('first digit')
-            	return false;
-        	} else {
-            	console.log('continue')
-        	}
-    	}
-    	// checks if character is uppercase
-    	else if ((charCode < 90) && (charCode > 65)) {
-        	console.log('continue')
-    	}
-    	// checks if character is lowercase
-    	else if ((charCode < 122) && (charCode > 96)) {
-        	console.log('continue')
-    	}
-    	// the character is a symbol
-    	else {
-        	symbol = true;
-        	return false;
-    	}
-    }
-
-    return true;
-}
-
-
-function validatePass() {
-    // check length of pass
-    if ((password.length < 8) || (password.length > 20)) {
-        return false;
-    }
-
-    // check if only letters & digits
-    for (var i = 0; i < password.length; i++) {
-        charCode = password.charCodeAt(i);
-
-        // checks if character is number
-        if ((charCode < 57) && (charCode > 48)) {
-            pwDigit = true
-        }
-        // checks if character is uppercase
-        else if ((charCode < 90) && (charCode > 64)) {
-            pwUpper = true
-        }
-        // checks if character is lowercase
-        else if ((charCode < 122) && (charCode > 96)) {
-            pwLower = true
-        }
-        // the character is a symbol
-        else {
-        	symbol = true
-            return false;
-        }
-    }
-
-    if (pwDigit && pwLower && pwUpper) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function validateRepeat() {
-    if (password == repeatPassword) {
-        return true
-    } else {
-        return false
+	if (validated == "user") {
+		alert("Username does not meet requirements");
+		location.reload();
+	} else if (validated == "pass") {
+		alert("Password does not meet requirements");
+		location.reload();
+	} else if (validated == "repeat") {
+		alert("Passwords do not match");
+		location.reload();
+	} else if (validated == "true") {		
+		register.type = "submit";
 	}
 }
 
 
+function validateForm() {
+	getInfo();
+
+	if (validateUser() && validatePass() && validateRepeat()) {
+		validated = "true";
+		return validated;
+	} else if (!validateUser()) {
+		validated = "user";
+		return validated;
+	} else if (!validatePass()) {
+		validated = "pass";
+		return validated;
+	} else if (!validateRepeat()) {
+		validated = "repeat";
+		return validated;
+	}
+
+}
+
+function validateUser() {
+	// check length of username
+	if ((user.length < 6) || (user.length > 20)) {
+		return false;
+	}
+
+	// check if only letters & digits
+	for (var i = 0; i < user.length; i++) {
+		charCode = user.charCodeAt(i);
+
+		// checks if character is number
+		if ((charCode < 57) && (charCode > 48)) {
+			// check first character of user
+			if (i == 0) {
+				console.log('first digit')
+				return false;
+			} else {
+				console.log('continue')
+			}
+		}
+		// checks if character is uppercase
+		else if ((charCode < 90) && (charCode > 65)) {
+			console.log('continue')
+		}
+		// checks if character is lowercase
+		else if ((charCode < 122) && (charCode > 96)) {
+			console.log('continue')
+		} 
+		// the character is a symbol
+		else {
+			symbol = true;
+			return false;
+		}
+	}
+
+	return true;
+}
 
 
+function validatePass() {
+	// check length of pass
+	if ((password.length < 8) || (password.length > 20)) {
+		return false;
+	}
+
+	// check if only letters & digits
+	for (var i = 0; i < password.length; i++) {
+		charCode = password.charCodeAt(i);
+
+		// checks if character is number
+		if ((charCode < 57) && (charCode > 48)) {
+			pwDigit = true
+		}
+		// checks if character is uppercase
+		else if ((charCode < 90) && (charCode > 64)) {
+			pwUpper = true
+		}
+		// checks if character is lowercase
+		else if ((charCode < 122) && (charCode > 96)) {
+			pwLower = true
+		} 
+		// the character is a symbol
+		else {
+			symbol = true
+			return false;
+		}
+	}
+
+	if (pwDigit && pwLower && pwUpper) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function validateRepeat() {
+	if (password == repeatPassword) {
+		return true
+	} else {
+		return false
+	}
+}
